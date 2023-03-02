3d6a2e957ff08f6e9b3451deab9f6c8f6fba1785
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-03-02 20:49:34 +1100

    refactor: Re-add OnMount component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <!-- SPDX-License-Identifier: MPL-2.0 -->
 <script>
   import '../app.scss';
+  import OnMount from '../components/OnMount.svelte';
 </script>
 
 <div class="prose prose-zinc">
+  <OnMount>
     <slot />
+  </OnMount>
 </div>
