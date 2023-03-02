66abca454901bbadcc9bbbcf34bc044616713538
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-03-03 09:35:09 +1100

    refactor: Update Privacy policy style

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -1,19 +1,21 @@
 <!-- SPDX-License-Identifier: MPL-2.0 -->
 <script>
-    import { sineIn } from 'svelte/easing';
-    import { fade } from 'svelte/transition';
+  import { sineIn } from 'svelte/easing';
+  import { fade } from 'svelte/transition';
 </script>
 
 <div
   class="w-screen h-screen flex flex-col justify-center items-start"
-  transition:fade={{duration: 500, easing: sineIn}}
+  transition:fade={{ duration: 500, easing: sineIn }}
 >
   <div class="m-20">
-    <!-- svelte-ignore a11y-invalid-attribute -->
-    <a href="#" on:click={() => history.back()} class="pb-10">Go Back</a>
-    <h1 class="text-white"><span>[Privacy Policy]</span></h1>
+    <div class="flex flex-row justify-start items-center place-items-baseline">
+      <h1 class="text-white"><span>[Privacy Policy]</span></h1>
+      <!-- svelte-ignore a11y-invalid-attribute -->
+      <a href="#" on:click={() => history.back()} class="pl-5">Go Back</a>
+    </div>
 
-    <div transition:fade={{duration: 1000, easing: sineIn}}>
+    <div transition:fade={{ duration: 1000, easing: sineIn }}>
       <p>
         This website uses Cloudflare, visit their <a
           href="https://www.cloudflare.com/privacypolicy/">Privacy Policy</a
@@ -22,8 +24,8 @@
 
       <p>
         This website does not store any data but this site uses Plausible Analytics for Analytical
-        purposes, we use a self-hosted instance @ analytics.uvvu.pw. This website does not track you in any way across websites
-        nor do we sell your Information to Third-Parties.
+        purposes, we use a self-hosted instance @ analytics.uvvu.pw. This website does not track you
+        in any way across websites nor do we sell your Information to Third-Parties.
       </p>
 
       <a
@@ -39,5 +41,14 @@
     padding: 0;
     color: #ff838d;
     @apply font-mono text-lg;
+  }
+  
+
+  a {
+    @apply font-mono;
+  }
+
+  h1 {
+    margin: 0;
   }
 </style>
