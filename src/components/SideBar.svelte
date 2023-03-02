7aa7987285274e1f3d06256b25f271cf72597d4f
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-03-02 13:16:58 +1100

    refactor: TODO: Privacy policy

--- src/components/SideBar.svelte
+++ src/components/SideBar.svelte
@@ -1,7 +1,13 @@
-<div class="side-bar flex flex-col justify-center items-center ml-10">
+<script>
+
+    import { fade } from 'svelte/transition';
+
+</script>
+<div class="side-bar flex flex-col justify-center items-center ml-10" transition:fade={{duration: 1000}}>
   <h2>Side-bar</h2>
   <a href="https://github.com/ArtieFuzzz" class="link">Github</a>
-  
+  <a href="/" class="link">Privacy Policy</a>
+  <p>read if cute!</p>
 </div>
 
 <style lang="scss">
@@ -12,7 +18,7 @@
     @apply font-mono;
   }
 
-  div a .link {
-    padding-bottom: 10px;
+  .link {
+    margin-bottom: 10px;
   }
 </style>
