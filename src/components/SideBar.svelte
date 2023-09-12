b5a629c2c36a199800793883d3bfa95541f2de3b
♥ ArtieFuzzz ♥ <hey@astolfo.cafe>
2023-09-12 22:18:01 +1000

    chore: Move link

--- src/components/SideBar.svelte
+++ src/components/SideBar.svelte
@@ -10,8 +10,8 @@
   <h2>Side-Bar</h2>
   <a href="/projects">Projects</a>
   <a href="https://github.com/ArtieFuzzz">Github</a>
-  <a href="https://www.codewars.com/users/ArtieFuzzz">CodeWars</a>
   <a href="https://sr.ht/~artiefuzzz">sr.ht</a>
+  <a href="https://www.codewars.com/users/ArtieFuzzz">CodeWars</a>
   <a href="/contact.asc">PGP</a>
   <a href="mailto:hey@astolfo.cafe">Email me!</a>
   <a href="/privacy">Privacy Policy</a>
