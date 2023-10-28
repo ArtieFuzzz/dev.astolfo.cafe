90ac10645f0703a5987a5b809ea6235bffe0bc4d
♥ ArtieFuzzz ♥ <hey@astolfo.cafe>
2023-10-28 15:43:08 +1100

    refactor: Update privacy content

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -23,7 +23,7 @@
       </p>
 
       <p>
-        This website does not store any data but this site uses Umami for Analytical purposes, we
+        This website does not store any data but this site uses Plausible for Analytical purposes, we
         use a self-hosted instance @ analytics.astolfo.cafe. This website does not track you in any
         way across websites nor do we sell your Information to Third-Parties.
       </p>
