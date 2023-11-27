320f623da17bc6131746d90ebe171447e3859e6c
♥ ArtieFuzzz ♥ <hey@astolfo.cafe>
2023-11-27 20:30:10 +1100

    chore(SideBar): Update styling
    Adds a hover transition

--- src/components/SideBar.svelte
+++ src/components/SideBar.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div
-  class="side-bar flex flex-col justify-center items-center ml-10"
+  class="flex flex-col items-center justify-center ml-10 side-bar"
   transition:fade={{ delay: 500, duration: 1000, easing: expoInOut }}
 >
   <h2>Side-Bar</h2>
@@ -18,10 +18,18 @@
 
 <style lang="scss">
   div a {
+    --bar-color: #007c72;
     text-decoration: none;
-    background-color: #007c72;
+    background-color: var(--bar-color);
     padding: 5px;
     @apply font-mono;
     margin-bottom: 10px;
+
+    transition: 1.2s cubic-bezier(0.075, 0.82, 0.165, 1);
+
+    &:hover {
+      --bar-color: white;
+      color: black;
+    }
   }
 </style>
